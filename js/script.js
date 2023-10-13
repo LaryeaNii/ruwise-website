a3617a2d058e3c565326a82642e757b36eff82f5
LaryeaNii <zadoklaryea@gmail.com>
2023-10-13 09:28:32 +0000

    search bar removed. info button fixed.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,12 @@
 let navbar = document.querySelector('.header .navbar');
-let searchForm = document.querySelector('.header .search-form');
-let loginForm = document.querySelector('.header .login-form');
 let contactInfo = document.querySelector('.contact-info');
 
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
+   
 };
 
-document.querySelector('#search-btn').onclick = () =>{
-   searchForm.classList.toggle('active');
-   navbar.classList.remove('active');
-   loginForm.classList.remove('active');
-};
 
-document.querySelector('#login-btn').onclick = () =>{
-   loginForm.classList.toggle('active');
-   navbar.classList.remove('active');
-   searchForm.classList.remove('active'); 
-};
 
 document.querySelector('#info-btn').onclick = () =>{
    contactInfo.classList.add('active');
@@ -31,14 +18,13 @@ document.querySelector('#close-contact-info').onclick = () =>{
 
 window.onscroll = () =>{
    navbar.classList.remove('active');
-   searchForm.classList.remove('active');
-   loginForm.classList.remove('active');
+  
    contactInfo.classList.remove('active');
 }
 
 var swiper = new Swiper(".home-slider", {
-   loop:true,
-   grabCursor:true,
+   loop: true,
+   grabCursor: true,
    navigation: {
      nextEl: ".swiper-button-next",
      prevEl: ".swiper-button-prev",
@@ -46,8 +32,8 @@ var swiper = new Swiper(".home-slider", {
 });
 
 var swiper = new Swiper(".reviews-slider", {
-   loop:true,
-   grabCursor:true,
+   loop: true,
+   grabCursor: true,
    spaceBetween: 20,
    breakpoints: {
       640: {
@@ -63,8 +49,8 @@ var swiper = new Swiper(".reviews-slider", {
 });
 
 var swiper = new Swiper(".blogs-slider", {
-   loop:true,
-   grabCursor:true,
+   loop: true,
+   grabCursor: true,
    spaceBetween: 20,
    breakpoints: {
       640: {
@@ -80,8 +66,8 @@ var swiper = new Swiper(".blogs-slider", {
 });
 
 var swiper = new Swiper(".logo-slider", {
-   loop:true,
-   grabCursor:true,
+   loop: true,
+   grabCursor: true,
    spaceBetween: 20,
    breakpoints: {
       450: {
